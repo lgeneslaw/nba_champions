de6dcab3395f015a90ef8e74a45ae4dea1cd55a8
Luke Geneslaw <lgeneslaw@gmail.com>
2013-03-19 20:45:04 -0400

    prints html of stats of every finals team

--- nbayears.py
+++ nbayears.py
@@ -1,21 +1,21 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-year = [0] * 66
+years = [0] * 66
 for i in range(0, 66):
-    year[i] = str(2012 - i)
+    years[i] = str(2012 - i)
 
 baseurl1 = "http://www.basketball-reference.com/leagues/NBA_"
 baseurl2 = "http://www.basketball-reference.com/leagues/BAA_"
-
+base = "http://www.basketball-reference.com"
+theurl = baseurl1
 champs = {}
 url = baseurl1
-for i in year:
-    if(int(i) == 1949):
-        print 'changing'
-        url = baseurl2
+for i in years:
     i = str(i)
-    url = url + i + ".html"
+    if(int(i) == 1949):
+        theurl = baseurl2
+    url = theurl + i + ".html"
     soup = BeautifulSoup(urllib2.urlopen(url))
     if(int(i) > 1970):
         table = soup.findAll("table")[3]
@@ -28,7 +28,15 @@ for i in year:
     for j in range(0, 2):
         links[j] = a[j]["href"]
 
-    print "\n\n" + i
-    for k in links:
-        print "\t" + k
+    champs[i] = links
 
+print "<html>\n<title>Championship Series</title>\n<body>"
+for i in years:
+    print "<p>" + i + "<br></p>"
+    for j in champs[i]:
+        print "\n<p><br>" + j + "<br></p>"
+        url = base + j
+        soup = BeautifulSoup(urllib2.urlopen(url))
+        table = soup.findAll("table")[4]
+        print table
+print "</body>\n</html>"
