e70bcc925304c7579525741f9aa242326affc91d
Luke Geneslaw <lgeneslaw@gmail.com>
2013-03-22 18:05:14 -0400

    added some year formatting

--- nbayears.py
+++ nbayears.py
@@ -11,6 +11,7 @@ base = "http://www.basketball-reference.com"
 theurl = baseurl1
 champs = {}
 url = baseurl1
+teams = [[""] * 2] * 66
 for i in years:
     i = str(i)
     if(int(i) == 1949):
@@ -26,17 +27,33 @@ for i in years:
     a = td.findAll("a")
     links = [""] * 2
     for j in range(0, 2):
+    #teams[2012 - int(i)] = [a[j].innerHtml, a[j]["href"]]
         links[j] = a[j]["href"]
 
     champs[i] = links
+    #print teams
 
 print "<html>\n<title>Championship Series</title>\n<body>"
+print "<head>\n<script style=\"text/css\">"
+print "p {font-size:36px;}"
+print "</script>"
+print "</head>"
 for i in years:
-    print "<p>" + i + "<br></p>"
+    print "<p align=\"center\">" + i + "<br></p>"
+    print "<table>"
+    print "<tr>"
     for j in champs[i]:
-        print "\n<p><br>" + j + "<br></p>"
+        print "<td align=\"center\"><b>" + j + "</b></td>"
+    print "</tr>"
+    print "<tr>"
+    for j in champs[i]:
+        print "<td>"
         url = base + j
         soup = BeautifulSoup(urllib2.urlopen(url))
         table = soup.findAll("table")[4]
         print table
+        print "</td>"
+    print "</tr>"
+    print "</table>"
 print "</body>\n</html>"
+
